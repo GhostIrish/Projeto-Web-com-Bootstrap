40839e90bc5ef3c1a8d35f362d516c9a7485b43e
Hariel <harielpimenta@hotmail.com>
2024-10-24 18:00:10 -0300

    Adicionando templates para começar a configurar as rotas

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.home import home_route
+from routes.cliente import cliente_route
 # url_for serve para montar url internas do nosso servidor web, de acordo com o nome da nossa func
 app = Flask(__name__)
 
 app.register_blueprint(home_route)
+app.register_blueprint(cliente_route, url_prefix='/clientes')
 
 app.run(debug=True)
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 cliente_route = Blueprint('cliente', __name__)
 
@@ -10,35 +10,41 @@ Rota de Clientes
     - /clientes/new (GET) - Renderizar um formulario para criar um cliente OK
     - /clientes/<id> (GET) - Obter um cliente pelo id OK
     - /clientes/<id>/edit (GET) - Renderiza um formulario para editar um cliente OK
-    - /clientes/<id>/update (PUT) - Atualizar os dados do cliente 
-    - /clientes/<id>/delete (DELETE) - Deleta o registro do usuário
+    - /clientes/<id>/update (PUT) - Atualizar os dados do cliente OK
+    - /clientes/<id>/delete (DELETE) - Deleta o registro do usuário OK
     
 """
 
-@cliente_route.route('/', method='GET')
+@cliente_route.route('/', methods=['GET'])
 def lista_clientes():
-    pass
-
-@cliente_route.route('/', method='POST')
+    '''Lista os clientes, é a pagina principal'''
+    return render_template('lista_clientes.html')
+@cliente_route.route('/', methods=['POST'])
 def insere_cliente():
+    '''Método que vai realizar a inserção do cliente na database'''
     pass
 
-@cliente_route.route('/novo_cliente', method='GET')
-def formulario_novo_cliente():
-    pass
+@cliente_route.route('/new')
+def form_novo_cliente():
+    '''formulario para cadastro de cliente'''
+    return render_template('form_cliente.html')
 
-@cliente_route.route('/<int:cliente_id>', method='GET') # obtem o id do cliente dinasmicamente
+@cliente_route.route('/<int:cliente_id>', methods=['GET']) # obtem o id do cliente dinasmicamente
 def obter_clientes(cliente_id):
+    '''exibe detalhes do cliente buscando pelo id'''
     pass
 
-@cliente_route.route('/editar/<int:cliente_id>')
-def formulario_editar_cliente(cliente_id):
+@cliente_route.route('/editar/<int:cliente_id>/edit')
+def form_editar_cliente(cliente_id):
+    '''renderiza o formulario para edição do cliente desejado'''
     pass
 
-@cliente_route.route('/<int:cliente_id>', method='PUT')
+@cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
+    '''Método que vai inserir essa atualização feita no formulário'''
     pass
 
-@cliente_route.route('/<int:cliente_id>', method='DELETE')
+@cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def deletar_cliente(cliente_id):
+    '''Deleta o cliente desejado'''
     pass
